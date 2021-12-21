928b59c27d202a762a8baf35f5a097fb8a0d2c4b
Hugo Castro <castroarch2002@gmail.com>
2021-12-21 17:23:07 +0000

    myfirstprogram & CreatingVariables
    I have created the first two programs for my practice. MyFirstProgram shows how C++ syntax works and CreatingVariables shows how we can use variables to output quicker

---
+++ C++ Learning W3S/C++ Learning/myfirstprogram.cpp
@@ -0,0 +1,11 @@
+#include <iostream> /* #include <iostream> is a header file library that works with 
+						input and output objects, such as cout. Header files add functionality to C++ programs */
+
+using namespace std; /* using namespace std means that we use names for objects and variables from the standard library */
+
+int main() {		/* This is called a function, Any code inside {} is executed */
+	cout << "Hello World!" << endl;	// This is a comment
+	cout << "I am learning C++";	/* An object used together with the insertion opoerator to output text*/
+	return 0;				/* return 0 ends the main function*/
+}						/* the curly bracket actually ends the main function */
+						/* This is a multi line comment */
